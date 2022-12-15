2852fe2f7478a5893cec8e3a85307bf3693d27b3
Quickscoper <nikitamarkov2u@gmail.com>
2022-12-15 19:19:25 +0500

    Реализация карт и колод

---
+++ BlackjackMain/PlayingCards.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using BlackjackMain.Properties;
+
+namespace BlackjackMain
+{
+    internal class Card
+    {
+        string pathTo;
+        public int value;    
+        public Card(int value,string path)
+        {
+            this.value = value;
+            pathTo = path;
+        }
+    }
+    internal class Deck
+    {
+        public List<Card> deckContainer = new List<Card>();        
+        public Deck()
+        {
+            foreach (var item in new DirectoryInfo(@"C:\Users\Quickscoper\Downloads\playngCards").GetFiles())
+            {
+                string path = item.FullName;
+                int value = Int32.Parse(Convert.ToString(item.Name[1]));
+
+                deckContainer.Add(new Card(value, path));
+            }
+            shuffle();
+        }
+
+        void shuffle()
+        {
+            Random rng = new Random();
+            int n = deckContainer.Count();
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card value = deckContainer[k];
+                deckContainer[k] = deckContainer[n];
+                deckContainer[n] = value;
+            }
+        }
+    }
+}
