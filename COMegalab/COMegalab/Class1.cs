d7ee201bab9277dbd532929cc2009c715c414b49
Quickscoper <nikitamarkov2u@gmail.com>
2022-12-18 14:35:41 +0500

    Добавлена заготовка COM-объекта

---
+++ COMegalab/COMegalab/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace COMegalab
+{
+    public interface blackjackHandler
+    {
+        public int handleRoundState(int player1Total,int player2Total, int player3Total)
+        {
+
+        }
+    }
+
+    public class Class1
+    {
+
+    }
+}
